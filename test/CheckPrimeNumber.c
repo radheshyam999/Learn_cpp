f668b20bb6dbf40f93d1ad421aa2aa696896e009
Radheshyam <34747415+radheshyam999@users.noreply.github.com>
2021-02-19 16:38:38 +0000

    Update CheckPrimeNumber.c
    there are some changes made

--- test/CheckPrimeNumber.c
+++ test/CheckPrimeNumber.c
@@ -1,8 +1,14 @@
 int isprime(int num){
-     int isprime =0;
-     if(num >2) isprime =1;
-     for(int i=2;i<=num/i;i++){
-               if(num%i==0) isprime=0; 
-     }
+      for(int i = 2; i <= num/i; i++) if(num%i==0) return 0;     
      return isprime;
 }
+prime = 0
+long long genralisPrime(long long number){
+     int i = 2;
+     while(i <= number % i){
+          if(number%i == 0) return 0;
+          i++;}
+                    
+return 1;
+}
+     
